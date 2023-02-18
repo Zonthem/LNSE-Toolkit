7eb7777985cdb22e1d89adf63c0c3180d3f2b8da
Zonthem <nico7.garaud@gmail.com>
2023-02-18 11:45:33 +0100

    test d'un pc a l'autre

---
+++ autoupdate.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET branche="main"
+ECHO Verification de la presence de mises a jour pour la version %branche%
+CALL git checkout %test%
+CALL git status | FIND /i "Your branch is up to date"
+if not errorlevel 1 (
+  echo Pas de mise a jour en attente
+) else (
+  CHOICE /M "Voulez-vous telecharger la mise a jour ?"
+  if errorlevel 2 GOTO END
+  CALL git fetch
+  CALL initialisation.bat
+)
+:END
