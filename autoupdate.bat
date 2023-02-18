a2f1074d13d8ee70f25283111929ddbc26171cfa
Zonthem <nico7.garaud@gmail.com>
2023-02-18 12:07:56 +0100

    ça devrait être bon

--- autoupdate.bat
+++ autoupdate.bat
@@ -2,8 +2,8 @@
 
 SET branche="main"
 ECHO Verification de la presence de mises a jour pour la version %branche%
-CALL git checkout %branche%
 CALL git fetch 
+CALL git checkout %branche%
 CALL git status | FIND /i "Your branch is up to date"
 if not errorlevel 1 (
   echo Pas de mise a jour en attente
